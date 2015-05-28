b226910132fde3d33e52fb994d2928dc5bdb9789
britg <brit@britg.com>
2015-05-28 09:32:42 -0500

    Get a scroll view working

---
+++ Assets/Scripts/UI/FeedView.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FeedView : MonoBehaviour {
+
+  public GameObject scrollerPrefab;
+
+	// Use this for initialization
+	void Start () {
+    GameObject scroller = Instantiate(scrollerPrefab);
+    scroller.transform.SetParent(transform, false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/UI/showfps.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class showfps : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+    Application.targetFrameRate = 60;
+    SmartConsole.ExecuteLine("show.fps True");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Vector3 angles = Camera.main.transform.localEulerAngles;
+    angles.y += Time.deltaTime * 5f;
+    Camera.main.transform.localEulerAngles = angles;
+	}
+}
