841e3d56528af7ca18fbcfb4efc8d9bb69f16bd7
britg <brit@britg.com>
2015-06-16 14:46:34 -0500

    Update input

---
+++ Assets/Scripts/Input/CameraController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : BaseBehaviour {
+
+  public Transform cameraTransform;
+
+  public float moveMultiplier = 0.1f;
+  public bool reverseX = true;
+  public bool reverseZ = true;
+  
+  public float zoomMultiplier = 1f;
+  public bool reverseZoom = true;
+  
+  public float defaultZoom = 50f;
+  public float minZoom = 10f;
+  public float maxZoom = 100f;
+
+	// Use this for initialization
+	void Start () {
+    cameraTransform = Camera.main.transform;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+  public void Move (Vector3 amount) {
+    var pos = cameraTransform.position;
+    pos.x += amount.x * (reverseX ? -1 : 1) * moveMultiplier;
+    pos.z += amount.y * (reverseZ ? -1 : 1) * moveMultiplier;
+    cameraTransform.position = pos;
+  }
+
+  public void Zoom (float amount) {
+    var pos = cameraTransform.position;
+    var newY = pos.y + amount * (reverseZoom ? -1 : 1) * zoomMultiplier;
+    newY = Mathf.Clamp(newY, minZoom, maxZoom);
+    pos.y = newY;
+    cameraTransform.position = pos;
+  }
+}
---
+++ Assets/Scripts/Input/MouseKeyboardCameraBehaviour.cs
@@ -0,0 +1,125 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MouseKeyboardCameraBehaviour : MonoBehaviour {
+
+  enum SessionType {
+    World,
+    UI
+  }
+
+  public CameraController cameraController;
+
+  public float sessionTravel = 1f;
+  SessionType sessionType;
+
+  Vector3 lastFramePosition;
+  float sessionDistance = 0f;
+  GameObject sessionObject;
+
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+    if (Input.GetMouseButtonDown(0)) {
+      CaptureMouseDown();
+    }
+
+	  if (Input.GetMouseButton(0)) {
+      CaptureMouseMove();
+    }
+
+    if (Input.GetMouseButtonUp(0)) {
+      CaptureMouseUp();
+    }
+
+    CaptureScroll();
+
+	}
+
+  void StartSession () {
+    sessionDistance = 0;
+    lastFramePosition = Input.mousePosition;
+
+    if (EventSystem.current.IsPointerOverGameObject()) {
+      sessionType = SessionType.UI;
+    } else {
+      sessionType = SessionType.World;
+    }
+  }
+
+  void CaptureMouseDown () {
+    StartSession();
+
+    if (sessionType == SessionType.World) {
+      CaptureWorldDown();
+    }
+
+  }
+
+  void CaptureWorldDown () {
+
+    RaycastHit hit;
+    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    if (Physics.Raycast(ray, out hit)) {
+      sessionObject = hit.collider.gameObject;
+      Debug.Log ("Down on " + sessionObject);
+    }
+  }
+
+  void CaptureMouseMove () {
+    if (sessionType == SessionType.UI) {
+
+    } else {
+      CaptureWorldMove();
+    }
+  }
+
+  void CaptureWorldMove () {
+    var diff = Input.mousePosition - lastFramePosition;
+    sessionDistance += diff.sqrMagnitude;
+    cameraController.Move(diff);
+    lastFramePosition = Input.mousePosition;
+  }
+
+  void CaptureMouseUp () {
+    if (sessionType == SessionType.UI) {
+
+    } else {
+      CaptureWorldUp();
+    }
+  }
+
+  void CaptureWorldUp () {
+    RaycastHit hit;
+    var ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+    if (Physics.Raycast(ray, out hit)) {
+      var upObject = hit.collider.gameObject;
+
+      if (upObject == sessionObject) {
+        if (sessionDistance <= sessionTravel) {
+          Debug.Log ("Registering as click " + upObject);
+        }
+      }
+    }
+  }
+
+  void CaptureScroll () {
+    StartSession();
+
+    if (sessionType == SessionType.World) {
+      CaptureGameScroll();
+    }
+  }
+
+  void CaptureGameScroll () {
+    var scrollAmount = Input.GetAxis("Mouse ScrollWheel");
+    cameraController.Zoom(scrollAmount);
+  }
+
+}
